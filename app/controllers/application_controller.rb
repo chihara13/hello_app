49123834d15597827ae5ca14713bd66a88145b23
chihara <bass_love_mini@ybb.ne.jp>
2018-09-17 10:01:25 +0900

    lesson03

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hola
-    render html: "hola, mundo!"
+  def hello
+    render html: "hello, world!"
   end
 
-  def goodbye
-    render html: "goodbye, world!"
-  end
 end
