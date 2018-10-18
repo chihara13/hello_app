db2ad7003cb6ac2554b831b1fdd8bca9e7a879c2
chihara <bass_love_mini@ybb.ne.jp>
2018-10-19 00:16:28 +0900

    7-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
-    #保存の成功
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.requir(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
