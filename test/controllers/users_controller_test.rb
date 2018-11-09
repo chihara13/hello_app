fa8f53601bf80daf5715648e55f31f376164b419
chihara <bass_love_mini@ybb.ne.jp>
2018-11-09 19:06:55 +0900

    lesson10 エラー

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ def setup
   @other_user = users(:archer)
 end
 
+test "shoud redirect index when logged in" do
+  get users_path
+  assert_redirected_to login_url
+end
+
   test "should get new" do
     get signup_path
     assert_response :success
